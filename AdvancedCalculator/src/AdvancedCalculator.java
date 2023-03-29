8670dced9a3ab3fb253d47d3bafe11e330c40bb7
ArminAfa <90157472+Armin-AF@users.noreply.github.com>
2023-03-29 09:43:06 +0200

    Adds more features to the calculator

--- AdvancedCalculator/src/AdvancedCalculator.java
+++ AdvancedCalculator/src/AdvancedCalculator.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.lang.Math;
+
 
 public class AdvancedCalculator {
 
@@ -15,52 +17,95 @@ public class AdvancedCalculator {
             System.out.println("3. Multiply");
             System.out.println("4. Divide");
             System.out.println("5. Exponentiation");
-            System.out.println("6. Exit");
-            System.out.print("Enter your choice (1-6): ");
+            System.out.println("6. Sine");
+            System.out.println("7. Cosine");
+            System.out.println("8. Tangent");
+            System.out.println("9. Logarithm (base 10)");
+            System.out.println("10. Natural Logarithm (base e)");
+            System.out.println("11. Factorial");
+            System.out.println("12. Exit");
+            System.out.print("Enter your choice (1-12): ");
             int choice = scanner.nextInt();
 
-            if (choice == 6) {
+            if (choice == 12) {
                 exit = true;
                 continue;
             }
 
-            System.out.print("Enter the first number: ");
-            double firstNumber = scanner.nextDouble();
+            if (choice >= 1 && choice <= 5) {
+                System.out.print("Enter the first number: ");
+                double firstNumber = scanner.nextDouble();
 
-            System.out.print("Enter the second number: ");
-            double secondNumber = scanner.nextDouble();
+                System.out.print("Enter the second number: ");
+                double secondNumber = scanner.nextDouble();
 
-            switch (choice) {
-                case 1:
-                    result = firstNumber + secondNumber;
-                    System.out.printf("Result: %.2f + %.2f = %.2f%n", firstNumber, secondNumber, result);
-                    break;
-                case 2:
-                    result = firstNumber - secondNumber;
-                    System.out.printf("Result: %.2f - %.2f = %.2f%n", firstNumber, secondNumber, result);
-                    break;
-                case 3:
-                    result = firstNumber * secondNumber;
-                    System.out.printf("Result: %.2f * %.2f = %.2f%n", firstNumber, secondNumber, result);
-                    break;
-                case 4:
-                    if (secondNumber == 0) {
-                        System.out.println("Cannot divide by zero");
+                switch (choice) {
+                    case 1:
+                        result = firstNumber + secondNumber;
+                        System.out.printf("Result: %.2f + %.2f = %.2f%n", firstNumber, secondNumber, result);
                         break;
-                    }
-                    result = firstNumber / secondNumber;
-                    System.out.printf("Result: %.2f / %.2f = %.2f%n", firstNumber, secondNumber, result);
-                    break;
-                case 5:
-                    result = Math.pow(firstNumber, secondNumber);
-                    System.out.printf("Result: %.2f ^ %.2f = %.2f%n", firstNumber, secondNumber, result);
-                    break;
-                default:
-                    System.out.println("Invalid choice");
+                    case 2:
+                        result = firstNumber - secondNumber;
+                        System.out.printf("Result: %.2f - %.2f = %.2f%n", firstNumber, secondNumber, result);
+                        break;
+                    case 3:
+                        result = firstNumber * secondNumber;
+                        System.out.printf("Result: %.2f * %.2f = %.2f%n", firstNumber, secondNumber, result);
+                        break;
+                    case 4:
+                        if (secondNumber == 0) {
+                            System.out.println("Error: Division by zero.");
+                        } else {
+                            result = firstNumber / secondNumber;
+                            System.out.printf("Result: %.2f / %.2f = %.2f%n", firstNumber, secondNumber, result);
+                        }
+                        break;
+                    case 5:
+                        result = Math.pow(firstNumber, secondNumber);
+                        System.out.printf("Result: %.2f ^ %.2f = %.2f%n", firstNumber, secondNumber, result);
+                        break;
+                }
+            } else {
+                System.out.print("Enter the number: ");
+                double number = scanner.nextDouble();
+                switch (choice) {
+                    case 6:
+                        result = Math.sin(Math.toRadians(number));
+                        System.out.printf("Result: sin(%.2f) = %.2f%n", number, result);
+                        break;
+                    case 7:
+                        result = Math.cos(Math.toRadians(number));
+                        System.out.printf("Result: cos(%.2f) = %.2f%n", number, result);
+                        break;
+                    case 8:
+                        result = Math.tan(Math.toRadians(number));
+                        System.out.printf("Result: tan(%.2f) = %.2f%n", number, result);
+                        break;
+                    case 9:
+                        result = Math.log10(number);
+                        System.out.printf("Result: log10(%.2f) = %.2f%n", number, result);
+                        break;
+                    case 10:
+                        result = Math.log(number);
+                        System.out.printf("Result: log(%.2f) = %.2f%n", number, result);
+                        break;
+
+                        case 11:
+                        result = factorial(number);
+                        System.out.printf("Result: %.2f! = %.2f%n", number, result);
+                        break;
+                }
             }
         }
+        scanner.close();
+    }
 
-        System.out.println("Goodbye!");
+    public static double factorial(double number) {
+        if (number == 0 || number == 1) {
+            return 1;
+        } else {
+            return number * factorial(number - 1);
+        }
     }
 }
 
